23f6db7537f057e1af201195a78cf5a78016f870
Devesh <ahirwardevesh298@gmail.com>
2025-07-28 21:45:25 +0530

    created User registration methods

---
+++ src/main/java/com/finance/finance_dashboard/controllers/AuthController.java
@@ -0,0 +1,24 @@
+package com.finance.finance_dashboard.controllers;
+
+import com.finance.finance_dashboard.entity.User;
+import com.finance.finance_dashboard.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        // Basic validation can be added
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.ok(savedUser);
+    }
+}
---
+++ src/main/java/com/finance/finance_dashboard/service/UserService.java
@@ -0,0 +1,23 @@
+package com.finance.finance_dashboard.service;
+
+import com.finance.finance_dashboard.entity.User;
+import com.finance.finance_dashboard.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
